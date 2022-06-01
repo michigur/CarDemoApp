fc929dd173abe1f2cac759b4e50240aa44be4944
User <User@LAB3-1>
2022-06-01 10:16:54 +0300

    Add project files.

---
+++ CarDemoApp/CarDemoApp/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CarDemoApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+
+        public static bool IsDevEnv
+        {
+            get
+            {
+                return false; //change this before release!
+            }
+        }
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
